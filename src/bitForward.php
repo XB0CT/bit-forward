3771553cc759c4e6f4973c9408929d50daff1c8b
XB0CT <XB0CT@mail.ru>
2020-02-11 18:15:34 +0300

    u
    Signed-off-by: XB0CT <XB0CT@mail.ru>

--- src/bitForward.php
+++ src/bitForward.php
@@ -10,10 +10,12 @@ use BN\BN;
 
 class Transport {
 	private $privateKey;
+	private $curlOpt;
 	function __construct($options = null) {
 		$this->ec = new EC('secp256k1');
 		$this->b58 = new Base58();
 		if ( isset($options['private']) ) $this->keyFromPrivate($options['private']);
+		if ( isset($options['curl']) ) $this->curlOpt = $options['curl'];
 	}
 	public function hash256($msg) {
 		return hash('sha256', hash('sha256', $msg, true), true);
@@ -36,7 +38,9 @@ class Transport {
 		$signature = $this->privateKey->sign(bin2hex($hash), ["k" => function ($iter) { 
 			return new BN(gmp_strval(gmp_random(512), '10'));
 		}]);
-		return $this->b58->encode(hex2bin(bin2hex($signature->recoveryParam).$signature->r->toString('hex').$signature->s->toString('hex')));
+		$r = $signature->r->toString('hex');
+		$s = $signature->s->toString('hex');
+		return $this->b58->encode(hex2bin(bin2hex($signature->recoveryParam).$r.$s));
 	}
 	public function pubFromSignature($msg, $signature) {
 		$signbin = $this->b58->decode($signature);
@@ -64,17 +68,17 @@ class Transport {
 		$pubkey = $this->privateKey->getPublic(true);
 		$pubenc = hex2bin($pubkey->encode("hex", true));
 		$pubhash = $this->hash160($pubenc);
-		echo "public       : ".$this->b58->encode("\x00".$pubhash.$this->checkSum($pubhash))."\n";
+		//echo "public       : ".$this->b58->encode("\x00".$pubhash.$this->checkSum($pubhash))."\n";
 
 		$script = "\x00"."\x14".$pubhash;
 		$scripHash = $this->hash160($script);
 		$checksum = $this->checkSum($scripHash);
 		$address = $this->b58->encode("\x05".$scripHash.$checksum);
-		echo "redeemScript : ".bin2hex($script)." [".strlen($script)."]\n";
+		//echo "redeemScript : ".bin2hex($script)." [".strlen($script)."]\n";
 		
 		$script = "\xA9"."\x14".$scripHash."\x87";
-		echo "scriptPubKey : ".bin2hex($script)." [".strlen($script)."]\n";
-		echo "address      : {$address}\n";
+		//echo "scriptPubKey : ".bin2hex($script)." [".strlen($script)."]\n";
+		//echo "address      : {$address}\n";
 
 		$msg = json_encode($params);
 		$this->errorMessage = "";
@@ -91,15 +95,20 @@ class Transport {
 			'sign' => $this->sign($msg)
 		]);
 		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, true);
+		foreach ($this->curlOpt as $k => $v) {
+			curl_setopt($ch, $k, $v);
+		}
 		
 		//$this->pm->start('curl_exec');
 		$recv = curl_exec($ch);
+
+		//var_dump($recv);
 		$code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
 		if ( $code != 200 ) {
 			$this->errorMessage = $recv;
 			$this->errorCode    = $code;
 			return false;
 		}
-		var_dump($recv);
+		return $recv;
 	}
 }
